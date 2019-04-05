947a8061ca9f199bf210f507b617a84094eb9440
Jason Rebelo <igotinfected@gmail.com>
2019-04-06 00:22:51 +0200

    Update Content folders for each platform

--- CrawlIT/CrawlIT.Shared/Content/Maps/bluegrey.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="bluegrey" tilewidth="32" tileheight="32" tilecount="2" columns="2">
- <image source="maptiles.png" width="64" height="32"/>
-</tileset>
